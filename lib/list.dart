1aeea5c4b67431e335ae168574cea9ab88c1ec77
cr34t1ve <cr34t1ve@protonmail.com>
2021-06-29 00:08:13 +0000

    list done

---
+++ lib/list.dart
@@ -0,0 +1,27 @@
+var catergory = ['school', 'personal'];
+
+class Tasks {
+  String? catergory;
+  String? taskName;
+
+  Tasks({this.taskName, this.catergory});
+}
+
+List<Tasks> taskList = [
+  Tasks(
+    catergory: catergory[0],
+    taskName: 'Submit assignment',
+  ),
+  Tasks(
+    catergory: catergory[0],
+    taskName: 'Write case study for App',
+  ),
+  Tasks(
+    catergory: catergory[0],
+    taskName: 'Send project link',
+  ),
+  Tasks(
+    catergory: catergory[1],
+    taskName: 'Take groceries to room',
+  ),
+];
--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,302 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:safo_todo/list.dart';
+import 'package:safo_todo/sizeConfig.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+    theme: ThemeData(
+      appBarTheme: AppBarTheme(
+        brightness: Brightness.light,
+      ),
+    ),
+    debugShowCheckedModeBanner: false,
+    home: Home(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    SizeConfig().init(context);
+    return Scaffold(
+      backgroundColor: Color(0xFFF1F1F1),
+      body: Stack(
+        children: [
+          Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20.0),
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: getProportionateScreenHeight(89.0),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Kwabena's Todo",
+                          style: TextStyle(
+                              fontFamily: 'Poppins',
+                              fontSize: 25.0,
+                              fontWeight: FontWeight.w500,
+                              color: Colors.black),
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: getProportionateScreenHeight(9.0),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Today's Tasks",
+                          style: TextStyle(
+                              fontFamily: 'Poppins',
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.w500,
+                              color: Colors.black),
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: getProportionateScreenHeight(70.0),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                height: 107,
+                child: ListView(
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    SizedBox(
+                      width: getProportionateScreenWidth(19.0),
+                    ),
+                    Center(
+                      child: Container(
+                        width: getProportionateScreenWidth(195.0),
+                        height: getProportionateScreenHeight(107),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(
+                              horizontal: getProportionateScreenWidth(29.0),
+                              vertical: getProportionateScreenHeight(16.0)),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                '13 tasks',
+                                style: TextStyle(
+                                    fontFamily: 'Poppins',
+                                    fontSize: getProportionateScreenWidth(15),
+                                    fontWeight: FontWeight.w400,
+                                    color: Colors.black),
+                              ),
+                              Text(
+                                'School',
+                                style: TextStyle(
+                                    fontFamily: 'Poppins',
+                                    fontSize: getProportionateScreenWidth(25),
+                                    fontWeight: FontWeight.w700,
+                                    color: Colors.black),
+                              ),
+                              SizedBox(
+                                height: getProportionateScreenHeight(3.0),
+                              ),
+                              Container(
+                                width: getProportionateScreenWidth(86),
+                                height: getProportionateScreenHeight(6),
+                                decoration:
+                                    BoxDecoration(color: Color(0xFF3FD972)),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: getProportionateScreenWidth(29.0),
+                    ),
+                    Center(
+                      child: Container(
+                        width: getProportionateScreenWidth(195.0),
+                        height: getProportionateScreenHeight(107),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(
+                              horizontal: getProportionateScreenWidth(29.0),
+                              vertical: getProportionateScreenHeight(16.0)),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                '5 tasks',
+                                style: TextStyle(
+                                    fontFamily: 'Poppins',
+                                    fontSize: getProportionateScreenWidth(15),
+                                    fontWeight: FontWeight.w400,
+                                    color: Colors.black),
+                              ),
+                              Text(
+                                'Personal',
+                                style: TextStyle(
+                                    fontFamily: 'Poppins',
+                                    fontSize: getProportionateScreenWidth(25),
+                                    fontWeight: FontWeight.w700,
+                                    color: Colors.black),
+                              ),
+                              SizedBox(
+                                height: getProportionateScreenHeight(3.0),
+                              ),
+                              Container(
+                                width: getProportionateScreenWidth(86),
+                                height: getProportionateScreenHeight(6),
+                                decoration:
+                                    BoxDecoration(color: Color(0xFFFF5BE4)),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20.0),
+                child: ListView.separated(
+                  itemCount: taskList.length,
+                  shrinkWrap: true,
+                  separatorBuilder: (BuildContext context, int index) =>
+                      SizedBox(
+                    height: getProportionateScreenHeight(25),
+                  ),
+                  itemBuilder: (context, index) => TaskCard(
+                    title: taskList[index].taskName,
+                    taskColor: taskList[index].catergory == 'school'
+                        ? Color(0xFF3FD972)
+                        : Color(0xFFFF5BE4),
+                  ),
+                ),
+              )
+            ],
+          ),
+          Positioned(
+            bottom: getProportionateScreenHeight(38),
+            right: getProportionateScreenWidth(23),
+            child: Container(
+              height: getProportionateScreenHeight(71),
+              width: getProportionateScreenWidth(71),
+              child: FloatingActionButton(
+                  onPressed: () {},
+                  backgroundColor: Color(0xFF1510F0),
+                  child: IconButton(
+                    onPressed: () {},
+                    iconSize: 41,
+                    icon: Icon(
+                      Icons.add,
+                      color: Colors.white,
+                    ),
+                  )),
+            ),
+          )
+        ],
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class TaskCard extends StatefulWidget {
+  const TaskCard({
+    Key? key,
+    this.title,
+    this.stateSetter,
+    this.taskColor,
+  }) : super(key: key);
+  final String? title;
+  final VoidCallback? stateSetter;
+  final Color? taskColor;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _TaskCardState createState() => _TaskCardState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _TaskCardState extends State<TaskCard> {
+  bool isSelected = false;
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          isSelected = !isSelected;
+          print(isSelected);
+        });
+      },
+      child: AnimatedContainer(
+        width: isSelected ? 0 : getProportionateScreenWidth(329.0),
+        height: isSelected ? 0 : getProportionateScreenHeight(65.0),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        duration: Duration(milliseconds: 200),
+        child: Row(
+          children: [
+            SizedBox(
+              width: getProportionateScreenWidth(15.0),
+            ),
+            Container(
+              width: getProportionateScreenWidth(20.0),
+              height: getProportionateScreenHeight(20.0),
+              decoration: BoxDecoration(
+                  border: Border.all(color: widget.taskColor!, width: 3),
+                  shape: BoxShape.circle),
+              child: Visibility(
+                visible: isSelected,
+                child: Container(
+                  color: widget.taskColor!,
+                  child: Center(
+                    child: Icon(
+                      Icons.check,
+                      size: 15,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              width: getProportionateScreenWidth(18.0),
             ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+              widget.title!,
+              style: TextStyle(
+                  fontFamily: 'Poppins',
+                  fontSize: getProportionateScreenWidth(15),
+                  fontWeight: FontWeight.w400,
+                  color: Colors.black),
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
